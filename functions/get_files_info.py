860c4c330acebac7ab4972d995a177aac202e924
QuimbanoPrime <qaz027@pm.me>
2026-01-05 20:29:20 -0800

    working on Get Files exercise

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,1 +1,18 @@
 def get_files_info(working_directory, directory="."):
+    import os
+
+    abs_path_working_dir = os.path.abspath(working_directory)
+    target_directory = os.path.normpath(os.path.join(abs_path_working_dir, directory))
+    validate_directory = os.path.commonpath([abs_path_working_dir, target_directory]) == abs_path_working_dir
+    if not validate_directory:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.exists(target_directory):
+        return f'Error: "{directory}" does not exist.'
+    
+    if directory == ".":
+        print(f"Result for current directory:")
+    else:
+        print(f"Result for '{directory}' directory:")
+    for items in os.listdir(target_directory):
+        print(f"- {items}, file_size={os.path.getsize(os.path.join(target_directory, items))} bytes, is_dir={os.path.isdir(os.path.join(target_directory, items))}")
+        #print(f"- {items}: file_size={os.path.getsize(items)} bytes, is_dir={os.path.isdir(items)}")
---
+++ test_get_files_info.py
@@ -0,0 +1,4 @@
+from functions.get_files_info import get_files_info
+
+get_files_info("calculator", ".")
+get_files_info("calculator", "/bin")
