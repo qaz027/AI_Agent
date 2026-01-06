622fdaa9fa169ea1d2fa288d89a384b8ef44c5fd
QuimbanoPrime <qaz027@pm.me>
2026-01-05 20:44:04 -0800

    adjustments to get file exercise - couldn't figure this out.

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,4 +1,23 @@
 from functions.get_files_info import get_files_info
 
-get_files_info("calculator", ".")
-get_files_info("calculator", "/bin")
+def test():
+    result = get_files_info("calculator", ".")
+    print("Result for current directory:")
+    print(result)
+    print("")
+
+    result = get_files_info("calculator", "pkg")
+    print("Result for 'pkg' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "/bin")
+    print("Result for '/bin' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "../")
+    print("Result for '../' directory:")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
