d6ef7c9adc4ac6506007927c638fcf0b7c206696
QuimbanoPrime <qaz027@pm.me>
2026-01-06 22:07:37 -0800

    adding get_file_content.py and associated code

---
+++ functions/get_file_content.py
@@ -0,0 +1,26 @@
+
+import os
+
+def get_file_content(working_directory, file_path):
+    try:
+        abs_path_working_dir = os.path.abspath(working_directory)
+        target_file_path = os.path.normpath(os.path.join(abs_path_working_dir, file_path))
+        validate_file = os.path.commonpath([abs_path_working_dir, target_file_path]) == abs_path_working_dir
+
+        if not validate_file:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.exists(target_file_path):
+            return f'Error: "{file_path}" does not exist.'
+        if not os.path.isfile(target_file_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+
+        with open(target_file_path, 'r') as file:
+            content = file.read(size = 10000)
+
+            # After reading the first MAX_CHARS...
+            if file.read(1):
+                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'        
+        return content
+    
+    except Exception as e:
+        return f"Error reading file: {e}"
---
+++ test_get_file_content.py
@@ -0,0 +1,8 @@
+from functions.get_file_content import get_file_content
+
+def test():
+    get_file_content("calculator", "lorem.txt")
+
+
+if __name__ == "__main__":
+    test()
