7d56f6426b87caa851f8e89c187ad5a41396ada6
QuimbanoPrime <qaz027@pm.me>
2026-01-11 21:41:41 -0800

    initiating exercise in calling functions

--- call_functions.py
+++ call_functions.py
@@ -11,3 +11,35 @@ available_functions = types.Tool(
         schema_run_python_file,
         schema_get_file_content],
 )
+
+
+def call_function(function_call, verbose=False):
+
+    function_name = function_call.name or ""
+
+    function_map = {
+        "get_file_content": get_file_content,
+        "write_file": write_file,
+        "run_python_file": run_python_file,
+        "get_file_info": get_file_info,
+        }
+
+    if verbose:
+        print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(f" - Calling function: {function_call.name}")
+
+    if not function_map[function_name]:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                    )
+                ],
+            )
+    
+    # here function_name is valid - shallow copy of function_call.args
+    args = dict(function_call.args) if function_call.args else {}
+    args["working_directory"] = "./calculator"
