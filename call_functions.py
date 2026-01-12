53d701515508905e1c5c360701f325e2ea286c80
QuimbanoPrime <qaz027@pm.me>
2026-01-11 21:58:36 -0800

    some more progress on exercise in calling functions

--- call_functions.py
+++ call_functions.py
@@ -43,3 +43,15 @@ def call_function(function_call, verbose=False):
     # here function_name is valid - shallow copy of function_call.args
     args = dict(function_call.args) if function_call.args else {}
     args["working_directory"] = "./calculator"
+
+    function_result = function_map[function_name](**args)
+
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
